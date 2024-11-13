626bf711ea766ea54d74382e2b7787471c1686d6
= <luis019dell@gmail.com>
2024-11-13 17:53:06 -0600

    manejo de eventos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import eventos from './eventos.vue'
 
-createApp(App).mount('#app')
+createApp(eventos).mount('#app')
